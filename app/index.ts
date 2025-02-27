8cbd24f16b2af2832b3d100228a7a246a140de13
Pat Heard <patrick.heard@cds-snc.ca>
2025-02-27 08:36:16 -0500

    fix: remove files that are no longer needed

--- app/index.ts
+++
@@ -1,9 +0,0 @@
-import config from "./config";
-import { createApp } from "./app";
-
-const app = createApp();
-
-app.listen(config.port, () => {
-  console.log(`Server: http://localhost:${config.port}`);
-  console.log(`WordPress: ${config.wordpress.url}`);
-});
