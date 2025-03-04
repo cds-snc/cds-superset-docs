d9e7e5cb4a3cb2053172c25595281c90111171f4
Pat Heard <patrick.heard@cds-snc.ca>
2025-03-04 20:49:21 +0000

    feat: add robots.txt file
    Add a robots.txt that blocks all search indexing.
    
    Also updates the PR workflow to use Makefile targets.

--- app/cmd/server/main.go
+++ app/cmd/server/main.go
@@ -38,6 +38,7 @@ func main() {
 
 	// Set up routes
 	http.Handle("/static/", http.StripPrefix("/static/", handlers.NewStaticHandler("static")))
+	http.Handle("/robots.txt", middleware.SecurityHeaders(handlers.NewRobotsHandler()))
 	http.Handle("/", middleware.SecurityHeaders(handlers.NewPageHandler(siteNames, wordPressClient)))
 
 	// Determine if this is a Lambda or HTTP server startup
