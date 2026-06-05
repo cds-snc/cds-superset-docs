87d9eb8e85d1e58b219bd845e4f25858e2aac174
Pat Heard <patrick.heard@cds-snc.ca>
2026-06-05 13:10:02 -0400

    chore: upgrade TF modules with tag change
    Upgrade to the latest version of the TF modules which has the ssc_cbrid tag change.

--- terragrunt/aws/oidc.tf
+++ terragrunt/aws/oidc.tf
@@ -4,7 +4,7 @@ locals {
 }
 
 module "github_workflow_roles" {
-  source = "github.com/cds-snc/terraform-modules//gh_oidc_role?ref=v10.11.4"
+  source = "github.com/cds-snc/terraform-modules//gh_oidc_role?ref=v11.3.5"
 
   roles = [
     {
