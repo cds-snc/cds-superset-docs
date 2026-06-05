8a96ac1e7577ad1a11a944eda3aa35f7d21fb27a
Pat Heard <patrick.heard@cds-snc.ca>
2026-06-05 18:47:24 +0000

    fix: fmt

--- terragrunt/aws/locals.tf
+++ terragrunt/aws/locals.tf
@@ -2,7 +2,7 @@ locals {
   common_tags = {
     Terraform  = "true"
     CostCentre = var.billing_code
-    ssc_cbrid = "22DI"
+    ssc_cbrid  = "22DI"
   }
 
   core_tags = merge(local.common_tags, {
